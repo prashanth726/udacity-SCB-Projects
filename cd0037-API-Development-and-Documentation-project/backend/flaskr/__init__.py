9a8efe71ba3bfb6fcb5d89307b0ab6b2a4a3c07f
Prashanth <prashanth726@gmail.com>
2022-08-26 14:46:12 +0530

    Enable CORS

--- cd0037-API-Development-and-Documentation-project/backend/flaskr/__init__.py
+++ cd0037-API-Development-and-Documentation-project/backend/flaskr/__init__.py
@@ -24,7 +24,7 @@ def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
     setup_db(app)
-    # CORS(app)
+    CORS(app)
 
     # CORS Headers
     @app.after_request
