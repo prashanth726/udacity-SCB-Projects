05176ad2bfc165cdd0b8f5f73d1e68af25e4843c
Prashanth <prashanth726@gmail.com>
2022-09-15 15:52:22 +0530

    Update angular project with components

--- my-store-udacity-project/src/app/app.module.ts
+++ my-store-udacity-project/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductItemDetailComponent } from './product-item-detail/product-item-detail.component';
+import { ProductItemComponent } from './product-item/product-item.component';
+import { CartComponent } from './cart/cart.component';
+import { ConfirmationComponent } from './confirmation/confirmation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductItemDetailComponent,
+    ProductItemComponent,
+    CartComponent,
+    ConfirmationComponent
   ],
   imports: [
     BrowserModule,
