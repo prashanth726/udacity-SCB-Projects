a7de928a52d053df543c1c13003d06b924e69809
Prashanth <prashanth726@gmail.com>
2022-09-25 19:01:01 +0530

    Update Routing

--- my-store-udacity-project/src/app/app-routing.module.ts
+++ my-store-udacity-project/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductItemDetailComponent } from './components/product-item-detail/product-item-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path : '', component: ProductListComponent},{ path:'products/:id', component: ProductItemDetailComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
